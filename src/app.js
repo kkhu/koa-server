70c1e82d83e69a6d88052131c597b8629afcec3c
hh5526 <hh5526@hotmail.com>
2020-07-11 23:12:41 +0800

    1、删除了无用的插件 2、package.json里的包全部升级为最新版 3、增加proxy

--- src/app.js
+++ src/app.js
@@ -2,22 +2,25 @@
 const koa = require('koa');
 const router = require('koa-router')();
 const wsServer = require('ws').Server;
+const proxy = require('koa-proxy');
 const serve = require('koa-static');
 const compress = require('koa-compress');
-const webConfig = require('./conf/web').config;
+const config = require('./conf/web');
 const https = require("https");
 const fs = require("fs");
 const app = new koa();
 
-console.log('config: \n'+ JSON.stringify(webConfig));
+const proxyConfig = config.server.proxy;
 
-const wss = new wsServer({port: webConfig.server.socket.listen});
+console.log('config: \n'+ JSON.stringify(config.server));
 
-app.use(serve(webConfig.server.www, {
-	index: webConfig.server.index, 
+const wss = new wsServer({port: config.server.socket.listen});
+
+app.use(serve(config.server.www, {
+	index: config.server.index, 
 	maxage: (1000 * 60 * 30),
 	hidden: true,
-	gzip: webConfig.server.gzip	
+	gzip: config.server.gzip	
 }));
 
 app.use(router.routes());
@@ -25,11 +28,18 @@ app.use(router.allowedMethods());
 
 app.use(compress({
 	filter: function (content_type) {
-         return /javascript/i.test(content_type)
+        return /javascript/i.test(content_type)
     },
     threshold: 2048,
     flush: require('zlib').Z_SYNC_FLUSH
 }));
+
+if (proxyConfig.use) {
+    app.use(proxy({
+        host: proxyConfig.pass, 
+        match: proxyConfig.match      
+    }));
+}
 
 // Broadcast to all.
 wss.broadcast = function broadcast(data) {
@@ -45,13 +55,11 @@ wss.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log('msg:' + message);
         ws.send(message);
-        
         wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(message);
-        }
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
         });
-
     })
     ws.send(' inited... ');
 });
@@ -68,7 +76,7 @@ router.get('/login',  async (ctx, next) => {
     ctx.body = '{"result": {"succ": true, isLogin: true}}';
 });
 
-app.listen(webConfig.server.listen);
+app.listen(config.server.listen);
 
 const options = {
 	key: fs.readFileSync("./server.key", "utf8"),
@@ -76,6 +84,6 @@ const options = {
 };
 https.createServer(options, app.callback()).listen(443);
 
-console.log('socket.listen:' + webConfig.server.socket.listen + '\n' + 
-			'listen: ' 		 + webConfig.server.listen + '\n' + 
+console.log('socket.listen:' + config.server.socket.listen + '\n' + 
+			'listen: ' 		 + config.server.listen + '\n' + 
             'app started... ');
--- src/conf/web.js
+++ src/conf/web.js
@@ -1,5 +1,5 @@
 
-exports.config = {
+module.exports = {
 	server: {
 		listen: 80,
 		www: '../www/',
@@ -13,7 +13,7 @@ exports.config = {
 
 		proxy: {
 			use: false, // 是否使用代理
-			location: '', // 需求使用代理的目录,proxy_location可以是一个正则表达式字符串     /abc/
+			match: /sport/, // 需求使用代理的目录,match可以是一个正则表达式字符串     /abc/
 			pass: '', // 服务器地址 , http://192.168.169.123:8089
 			micro_service: '',
 			merge_mode: '',
